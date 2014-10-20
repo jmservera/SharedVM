c1ebbb3b7fadadeea87e4fe9d5e0da3c6c10b80c
jmservera <jmservera@outlook.com>
2014-10-21 00:07:30 +0200

    First demo mvvmlight + pcl

---
+++ Common/Player.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class Player:ObservableObject
+    {
+        private string _pictureUrl;
+
+        public string PictureUrl
+        {
+            get { return _pictureUrl; }
+            set { Set(ref _pictureUrl , value); }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { Set(ref _name , value); }
+        }
+
+    }
+}
---
+++ Common/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ViewModel
+{
+    public class MainViewModel:ObservableObject
+    {
+        private Player _selectedPlayer;
+
+        public Player SelectedPlayer
+        {
+            get { return _selectedPlayer; }
+            set { Set(ref _selectedPlayer , value); }
+        }
+
+        private List<Player> _players = new List<Player>();
+        public IEnumerable<Player> Players { get { return _players; } }
+
+        public MainViewModel()
+        {
+            //TODO: retrieve data from file
+            _players.Add(new Player { Name = "Kang", PictureUrl = "http://img3.wikia.nocookie.net/__cb20131208164609/simpsons/images/thumb/d/d4/Tapped_Out_Unlock_Kang.png/200px-Tapped_Out_Unlock_Kang.png" });
+            _players.Add(new Player { Name = "Bart", PictureUrl = "http://img4.wikia.nocookie.net/__cb20140110020448/simpsons/images/1/14/Bartsimpson2.png" });
+        }
+    }
+}
