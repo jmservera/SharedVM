45fedbf640a2d3a775d9893d3bf26b2100689fc0
Juan Manuel Servera <jmservera@outlook.com>
2014-10-22 09:11:44 +0200

    add models folder

--- Common/ViewModel/LoginViewModel.cs
+++ Common/ViewModel/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Common.Models;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace Common.ViewModel
 {
     public class LoginViewModel:ViewModelBase
     {
-                private Player _selectedPlayer;
+        private Player _selectedPlayer;
 
         public Player SelectedPlayer
         {
